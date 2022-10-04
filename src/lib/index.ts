1d7d339d4011a425bb736e08bc41485b54603dc8
AnnikenYT <anniken@mooonshine.net>
2022-10-04 17:28:00 -0300

    feat(core): :tada: init

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+// Reexport your entry components here
+import '../app.css'
+export * from './HelloWorld.svelte'
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import '../app.css'
+    export let data: LayoutData;
+</script>
+
+
+<div class="flex flex-col justify-center items-center min-h-screen w-full bg-clean-dark">
+    <slot/>
+</div>
